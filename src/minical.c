fd08588d8094322bb2a5a4a95df7fbf690e209ee
ahmere-s <csmail.santos@gmail.com>
2025-12-24 01:06:45 -0500

    3

--- src/minical.c
+++ src/minical.c
@@ -11,8 +11,26 @@ bool is_leap_year(int year){
 int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};  /* Number of days in each of the twelve
 months starting with index one for January ending on index twelve for December */
 
-void add_days_to_date(){
+void add_days_to_date(int* month, int* day, int* year, int days_left_to_add){
+  while (days_left_to_add > 0){
+    int dim_count = days_in_month[*month]; //get number of days using given param *month
+    
+    //update dim_count if month is feb AND its a leap year
+    if (*month == 2 && is_leap_year(*year) == true){dim_count += 1;}
+    int days_left_in_month = dim_count - *day;
 
+    if (days_left_to_add > days_left_in_month){
+      days_left_to_add -= (days_left_in_month + 1);
+      *day = 1;
+      //Return to January if its December, then increment the new year
+      if(*month == 12){*month = 1; *year += 1;}
+      else{*month += 1;} //Otherwise just increment the month
+    }
+    else{
+      *day += days_left_to_add;
+      days_left_to_add = 0;
+    }
+  }
 }
 
 int main(){
